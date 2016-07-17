f2fc2c8ed2896a3a356cd59b13cbd66427682d9f
Chirdeep Tomar <chirdeep.tomar@gmail.com>
2016-07-17 20:59:52 +0100

    Updated with fake MSN actor

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -4,10 +4,9 @@ import com.learning.actors.StockPriceRouter
 /**
   * Created by Chirdeep on 11/07/2016.
   */
-object App extends App{
+object App extends App {
   val actorSystem = ActorSystem("ActorSystem")
   val actor = actorSystem.actorOf(StockPriceRouter.props, "com.learning.actors.StockPriceRouter")
-
 
   actor ! "msft"
 
--- src/main/scala/com/learning/actors/StockPriceRouter.scala
+++ src/main/scala/com/learning/actors/StockPriceRouter.scala
@@ -4,24 +4,23 @@ import akka.actor.{Actor, Props}
 
 /**
   * Created by Chirdeep on 11/07/2016.
-  */
-
-object StockPriceRouter {
-  def props = Props(new StockPriceRouter)
-}
+**/
 
 class StockPriceRouter extends Actor {
 
   override def receive: Receive = {
-    case ticker:String => {
+    case ticker:String =>
       println(s"Firing requests to service apis for: $ticker")
       val yahooFinder = context.actorOf(Props[YahooFinanceFinder], "YahooFinanceFinder")
       val msnFinder = context.actorOf(Props[MsnFinanceFinder], "MsnFinanceFinder")
       yahooFinder ! ticker
       msnFinder ! ticker
       println(s"Fired requests to third party service apis")
-    }
 
     case _ => println("received unknown message")
   }
+}
+
+object StockPriceRouter {
+  def props = Props(new StockPriceRouter)
 }
